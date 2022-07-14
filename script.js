6928988a42d279ea80395bdb6aa7b451354b34ad
IdrisGuloyev <idrisguloyev@gmail.com>
2022-07-14 21:09:23 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
+const btnsOpenModalWindow = document.querySelectorAll(
+  '.btn--show-modal-window'
+);
+
+const openModalWindow = function (e) {
+  e.preventDefault();
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModalWindow = function () {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModalWindow.forEach(button =>
+  button.addEventListener('click', openModalWindow)
+);
+
+// for (let i = 0; i < btnsOpenModalWindow.length; i++)
+//   btnsOpenModalWindow[i].addEventListener('click', openModalWindow);
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+overlay.addEventListener('click', closeModalWindow);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+    closeModalWindow();
+  }
+});
+
+// Scroll to
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const section1Coords = section1.getBoundingClientRect();
+
+  console.log(section1Coords);
+  // window.scrollTo(
+  //   section1Coords.left + window.pageXOffset,
+  //   section1Coords.top + window.pageYOffset
+  // );
+
+  // Old method
+
+  // window.scrollTo({
+  //   left: section1Coords.left + window.pageXOffset,
+  //   top: section1Coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // New method
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
